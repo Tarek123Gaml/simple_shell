39bff79ae89ff8c847a8f8151f2f6c17560e0c90
Happykhamissa <elmansourikhamissa@gmail.com>
2023-09-06 13:57:47 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,74 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+/*Error status macros*/
+#define PERMISSION_DENIED 126
+#define EXIT_ERROR 2
+#define TOK_D " \t\r\n\a\""
+#define NOT_FOUND 127
+extern char **environ;
+
+/**here the macros**/
+#define NON_INTERACTIVE_PIPED 2
+#define INTERACTIVE 1
+#define NON_INTERACTIVE_FILE 0
+#define ERROR -1
+
+/**
+ * struct list_path - singly linked list
+ * @path: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_path
+{
+	char *path;
+	unsigned int len;
+	struct list_path *next;
+} list_path;
+
+/**string functions**/
+char *_strcat(char *dest, char *src);
+int _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t len);
+char *_strdup(char *str);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+
+
+int _putchar(char c);
+void _puts(char *str);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int count_input(char *str);
+int count_delims(char *str, char *del);
+char *remove_new_line(char *str);
+void signal_handler(int sig_id);
+void _open_help(void);
+void _print_env(void);
+void _handle_exit(char **u_tokns, char *line);
+int execBuiltInCommands(char **u_tokns, char *line);
+void frees_get_env(char *env_path);
+void frees_tokens(char **tokns);
+int exec(char *cname, char **opts);
+char *_getenv(const char *name);
+char **tokenize(char *str, char *del, int len);
+char *find(char *cname);
+
+
+#endif
+
+
