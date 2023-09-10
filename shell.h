f6ac990e02343ae2bae59eaf9478b6778d2dc19d
tarek <taga61188@gmail.com>
2023-09-10 01:57:43 -0700

    string functions

--- shell.h
+++ shell.h
@@ -1,74 +1,122 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <unistd.h>
-#include <signal.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
 #include <fcntl.h>
-#include <dirent.h>
+#include <errno.h>
 
-/*Error status macros*/
-#define PERMISSION_DENIED 126
-#define EXIT_ERROR 2
-#define TOK_D " \t\r\n\a\""
-#define NOT_FOUND 127
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
 extern char **environ;
 
-/**here the macros**/
-#define NON_INTERACTIVE_PIPED 2
-#define INTERACTIVE 1
-#define NON_INTERACTIVE_FILE 0
-#define ERROR -1
 
 /**
- * struct list_path - singly linked list
- * @path: string
- * @len: length of the string
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
  * @next: points to the next node
- *
- * Description: singly linked list node structure
  */
-typedef struct list_path
+typedef struct liststr
 {
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ *struct passinfo - contains pseudo-arguements to pass into a function,
+ *					allowing uniform prototype for function pointer struct
+ *@arg: a string generated from getline containing arguements
+ *@argv: an array of strings generated from arg
+ *@path: a string path for the current command
+ *@argc: the argument count
+ *@line_count: the error count
+ *@err_num: the error code for exit()s
+ *@linecount_flag: if on count this line of input
+ *@fname: the program filename
+ *@env: linked list local copy of environ
+ *@environ: custom modified copy of environ from LL env
+ *@history: the history node
+ *@alias: the alias node
+ *@env_changed: on if environ was changed
+ *@status: the return status of the last exec'd command
+ *@cmd_buf: address of pointer to cmd_buf, on if chaining
+ *@cmd_buf_type: CMD_type ||, &&, ;
+ *@readfd: the fd from which to read line input
+ *@histcount: the history line number count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
 	char *path;
-	unsigned int len;
-	struct list_path *next;
-} list_path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
 
-/**string functions**/
-char *_strcat(char *dest, char *src);
-int _strlen(const char *s);
-int _strcmp(char *s1, char *s2);
-int _strncmp(const char *s1, const char *s2, size_t len);
-char *_strdup(char *str);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
 
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
 
-int _putchar(char c);
+/**
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+int _strlen(char *s);
+int _strcmp(char *s ,char *s1);
+char *start_with(const char *s, const char *s1);
+char *_strcat(char *s, char *s1);
+char *_strcpy(char *s, char *s1);
+char *_strdup(const char *s);
 void _puts(char *str);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int count_input(char *str);
-int count_delims(char *str, char *del);
-char *remove_new_line(char *str);
-void signal_handler(int sig_id);
-void _open_help(void);
-void _print_env(void);
-void _handle_exit(char **u_tokns, char *line);
-int execBuiltInCommands(char **u_tokns, char *line);
-void frees_get_env(char *env_path);
-void frees_tokens(char **tokns);
-int exec(char *cname, char **opts);
-char *_getenv(const char *name);
-char **tokenize(char *str, char *del, int len);
-char *find(char *cname);
-
+int _putchar(char c);
 
 #endif
-
-
---
+++ str_func.c
@@ -0,0 +1,80 @@
+#include "shell.h"
+
+/**
+ * _strlen - function that return length of string
+ *
+ * @s: takes input as string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		count++;
+	return (count);
+}
+
+/**
+ * _strcmp - that function comparison tow string
+ *
+ * @s: takes first string
+ * @s1: takes second string
+ *
+ * Return: 1 or -1 or 0
+ */
+
+int _strcmp(char *s, char *s1)
+{
+	while (*s && *s1)
+	{
+		if (*s != *s1)
+			return (*s - *s1);
+		s++;
+		s1++;
+	}
+	if (*s == *s1)
+		return (0);
+	else
+		return (*s > *s1 ? 1 : -1);
+}
+
+/**
+ * start_with - checks if s1 starts with s
+ * @s: string to search
+ * @s1: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+
+char *start_with(const char *s, const char *s1)
+{
+	while (*s1)
+		if (*s1++ != *s++)
+			return (NULL);
+	return ((char *)s);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @s: takes fist string
+ * @s1: takes second string
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *s, char *s1)
+{
+	char *ret = s;
+
+	while (*s)
+		s++;
+	while (*s1)
+		*s++ = *s1++;
+	*s = *s1;
+	return (ret);
+}
