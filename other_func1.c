a815d340f2587d786881c14f2a30b8e5adfbf3b7
Happykhamissa <elmansourikhamissa@gmail.com>
2023-09-11 06:52:49 -0700

    other functions 1

---
+++ other_func1.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+
+/**
+ *
+ *
+ */
+char convert_number(long int num, int base, int flags)
+
+/**
+ *
+ *
+ */
+
+int _erratoi(char *s)
+
+
+/**
+ *
+ *
+ *
+ */
+
+void remove_comments(char *buf)
+
+/**
+ *
+ *
+ */
+
+void print_error(info_t *info, char *estr)
+
+/**
+ *
+ *
+ */
+
+int print_d(int input, int fd)
+
