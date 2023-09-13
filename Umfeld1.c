05da0185eaf70987f4fe02a63436c064842cb770
Happykhamissa <elmansourikhamissa@gmail.com>
2023-09-13 15:29:50 -0700

    environnement

---
+++ Umfeld1.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+
+/**
+ * novel_env - Initialize a new enviro var or modify an existing one
+ * @info: A structure to maintain constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ * Return: Always 0
+ */
+int novel_env(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
+
+/**
+ * delete_var - Remove an environment variable
+ * @info: A structure that maintain constant function prototype.
+ * @var: the string env var property
+ *
+ * Return: 1 on delete, 0 otherwise
+ */
+
+int delete_var(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+	2	return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * copy_envir - returns the string array copy of our environ
+ * @info: A structure to maintain constant function prototype.
+ * Return: Always 0
+ */
+char **copy_envir(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
