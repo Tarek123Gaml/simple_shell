a142267982c1143ca921b1e81d87075708d44828
Happykhamissa <elmansourikhamissa@gmail.com>
2023-09-17 13:16:18 -0700

    integrierte_Emulatoren.c

--- integrierte_Emulatoren.c
+++ integrierte_Emulatoren.c
@@ -11,16 +11,16 @@ int Sh_exit(info_t *info)
 
 	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		checkexit = _erratoi(info->argv[1]);
+		checkexit = err_atoi(info->argv[1]);
 		if (checkexit == -1)
 		{
 			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
+			print_Error(info, "Illegal number: ");
+			eput_str(info->argv[1]);
+			_eput_char('\n');
 			return (1);
 		}
-		info->err_num = _erratoi(info->argv[1]);
+		info->err_num = err_atoi(info->argv[1]);
 		return (-2);
 	}
 	info->err_num = -1;
@@ -58,36 +58,36 @@ int curnt_dir(info_t *info)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
-		dir = _getenv(info, "HOME=");
+		dir = get_var_env(info, "HOME=");
 		if (!dir)
 			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+				chdir((dir = get_var_env(info, "PWD=")) ? dir : "/");
 		else
 			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
-		if (!_getenv(info, "OLDPWD="))
+		if (!get_var_env(info, "OLDPWD="))
 		{
 			_puts(s);
 			_putchar('\n');
 			return (1);
 		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		_puts(get_var_env(info, "OLDPWD=")), _putchar('\n');
 		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+			chdir((dir = get_var_env(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
 	if (chdir_ret == -1)
 	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
+		print_Error(info, "can't cd to ");
+		eput_str(info->argv[1]), _eput_char('\n');
 	}
 	else
 	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
+		novel_env(info, "OLDPWD", get_var_env(info, "PWD="));
+		novel_env(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
 }
