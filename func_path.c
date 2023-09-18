0ff2434b22d17dd7f51944c753b8cfbf22bfbb9b
tarek <taga61188@gmail.com>
2023-09-17 20:56:48 -0700

    m

--- func_path.c
+++ func_path.c
@@ -57,16 +57,16 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 
 	if (!pathstr)
 		return (NULL);
-	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	if ((_strlen(cmd) > 2) && start_with(cmd, "./"))
 	{
-		if (is_cmd(info, cmd))
+		if (exe_cmd(info, cmd))
 			return (cmd);
 	}
 	while (1)
 	{
 		if (!pathstr[i] || pathstr[i] == ':')
 		{
-			path = dup_chars(pathstr, curr_pos, i);
+			path = duplic_carac(pathstr, curr_pos, i);
 			if (!*path)
 				_strcat(path, cmd);
 			else
@@ -74,7 +74,7 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 				_strcat(path, "/");
 				_strcat(path, cmd);
 			}
-			if (is_cmd(info, path))
+			if (exe_cmd(info, path))
 				return (path);
 			if (!pathstr[i])
 				break;
